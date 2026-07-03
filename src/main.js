3f7d34eec9f0306f0148632cd9f01abc57793805
MohammedAref11 <mohammedaref.business11@gmail.com>
2026-07-03 17:09:32 +0400

    added micro interaction for hero section and nav bar and fixed bugs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 const hamMenuEl = document.getElementById("hamMenu"); 
 const navBarEl = document.getElementById("navBar"); 
+const navMenu = document.getElementById("navMenu"); 
+
 
 hamMenuEl.addEventListener("click", (e) => { 
-    navBarEl.classList.toggle("clicked")
+    navBarEl.classList.toggle("clicked"); 
+
+    if (navBarEl.classList.contains("clicked")) { 
+        navMenu.style.height = navMenu.scrollHeight + "px";
+    } else { 
+        navMenu.style.height = "0px"; 
+    }
 })
+
+
+// Reading more about this depending on use case
+// window.addEventListener('resize', (e) => { 
+//     if (window.innerWidth > 765) { 
+//         navMenu.style.height = 'fit-content'; 
+//     }
+
+//     if (window.innerWidth < 765) { 
+//         navMenu.style.height = "0px"
+//     }
+// })
